ebbdf1d2fc6cdbb6add9be6eb801dba9e1ca87ee
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-12-20 09:37:31 -0800

    updating state and city class for sqlalchemy setup

--- models/city.py
+++ models/city.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+
+STORAGE = getenv("HBNB_TYPE_STORAGE")
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+
+    __tablename__ = 'cities'
+    if STORAGE == "db":
+        state_id = Column(String(60), ForeignKey('state_id'), nullable=False)
+        name = Column(String(128), nullable=False)
+    else:
+        state_id = ""
+        name = ""
--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from models.city import City
+from os import getenv
+from sqlalchemy.orm import relationship
 
+STORAGE = getenv("HBNB_TYPE_STORAGE")
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if STORAGE == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    else:
+        name = ""
+        
+        @property
+        def cities(self):
+            """Returns the cities"""
+            cities_list = []
+            from models import storage
+            for key, value in storage.all(City).items():
+                if value.state_id == self.id:
+                    cities_list.append(value)
+            return cities_list
