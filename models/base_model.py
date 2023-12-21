b370c486693f069fb44ec167d12ea678e6b31a01
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-12-20 17:40:02 -0800

    finish fixing bugs

--- models/base_model.py
+++ models/base_model.py
@@ -52,10 +52,10 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary.update({"__class__": (str(type(self))
+                                         .split(".")[-1]).split("'")[0]})
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
         if "_sa_instance_state" in dictionary.keys():
             del dictionary["_sa_instance_state"]
         return dictionary
--- models/city.py
+++ models/city.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
+import os
 
-STORAGE = getenv("HBNB_TYPE_STORAGE")
+STORAGE = os.getenv("HBNB_TYPE_STORAGE")
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """The city class, contains state ID and name"""
 
-    __tablename__ = 'cities'
+    __tablename__ = "cities"
     if STORAGE == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
