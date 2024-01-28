4f602f6c5050ad07eb0c8fcd7d4ea5500a18a166
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2024-01-28 07:11:35 -0800

    a script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Importing the flask module """
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello():
+        """ A funtion that defines a return value. """
+        return "Hello HBNB!"
+
+if __name__ == "__main__":
+        app.run()
