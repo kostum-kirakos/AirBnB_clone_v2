1c5023dc7b07f3e323e9ab3d98d5a609dbccb260
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2024-01-28 10:24:31 -0800

    advanced completed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['18.208.120.138', '54.237.53.64']
+env.hosts = ['54.237.53.64', '18.208.120.138']
 
 
 def do_deploy(archive_path):
