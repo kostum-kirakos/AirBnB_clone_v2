2c3dd4a0fbeb99a3cbdd3898b81b481c1d932afe
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2024-01-28 07:14:30 -0800

    code style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
-        """ A funtion that defines a return value. """
-        return "Hello HBNB!"
+    """ A funtion that defines a return value. """
+    return "Hello HBNB!"
+
 
 if __name__ == "__main__":
-        app.run()
+    app.run()
