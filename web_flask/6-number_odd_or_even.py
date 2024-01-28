ce2c8d01d125b9c31ea77010fbe1e52f7ba69f32
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2024-01-28 09:13:24 -0800

    display when its odd or even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+""" Importing the flask module """
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ A funtion that defines a return value. """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ A funtion that defines a return value. """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ A funtion that defines a return value. """
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pyth(text="is cool"):
+    """ A funtion that defines a return value. """
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_or_not(n):
+    """ A funtion that checks if number or not. """
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def display_html_page(n):
+    """ A funtion that checks display a page if number. """
+    if isinstance(n, int):
+        return render_template("5-number.html", num=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def numberEvenOdd(n):
+    """A function that renders a template if n was int"""
+    if isinstance(n, int):
+        if n % 2 == 0:
+            return render_template("6-number_odd_or_even.html",
+                                   num=n, state="even")
+        else
+            return render_template("6-number_odd_or_even.html",
+                                   num=n, state="odd")
+
+
+if __name__ == "__main__":
+    app.run()
