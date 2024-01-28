eef87f44e8bc993b6fa2ad1a37fe65daadd5ad79
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2024-01-28 08:05:42 -0800

    commiting a script that returns a path of python/

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Importing the flask module """
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ A funtion that defines a return value. """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ A funtion that defines a return value. """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ A funtion that defines a return value. """
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pyth(text="is cool"):
+    """ A funtion that defines a return value. """
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+if __name__ == "__main__":
+    app.run()
